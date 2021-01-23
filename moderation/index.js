4cd8d764da2b67aa39cecdb2f62c257effef3da4
Harun Kilic <harunkilic@live.dk>
2021-01-23 13:45:50 +0100

    Update

--- moderation/index.js
+++ moderation/index.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", async (req, res) => {
+app.post('/events', async (req, res) => {
   const { type, data } = req.body;
 
-  if (type === "CommentCreated") {
-    const status = data.content.includes("orange") ? "REJECTED" : "APPROVED";
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'REJECTED' : 'APPROVED';
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-service:4005/events', {
       type: 'CommentModerated',
       data: {
-        ...data,
+        id: data.id,
+        postId: data.postId,
         status,
+        content: data.content
       }
-    })
+    });
   }
 
   res.send({});
 });
 
 app.listen(4003, () => {
-  console.log("Listening on 4003");
+  console.log('Listening on 4003');
 });
